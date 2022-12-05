b151a82ee5ade2ccc02300e62be4cccdac16f477
Phyllis Kan <pyk9q7@gmail.com>
2022-12-05 12:01:32 -0600

    use item loader to make data clean

--- ApartmentsScraper/items.py
+++ ApartmentsScraper/items.py
@@ -4,9 +4,17 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
+
+def remove_dollar_symbol(value):
+    return value.replace('$', '').strip()
 
 class ApartmentsscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field()
-    pricing = scrapy.Field()
-    bed = scrapy.Field()
+    name = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    pricing = scrapy.Field(input_processor = MapCompose(remove_tags, remove_dollar_symbol), output_processor = TakeFirst())
+    bed = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    rent = scrapy.Field(input_processor = MapCompose(remove_tags, remove_dollar_symbol), output_processor = TakeFirst())
+    beds = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
